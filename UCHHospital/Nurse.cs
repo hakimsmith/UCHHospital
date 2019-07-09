916921711052b82c86793fb8438c4f9fd8adb5a2
ladejesus <luisito.dejesus@gmail.com>
2019-07-09 16:18:28 -0400

    added nurse and patient class

--- UCHHospital/Nurse.cs
+++ UCHHospital/Nurse.cs
@@ -4,8 +4,41 @@ using System.Text;
 
 namespace UCHHospital
 {
-    class Nurse
+    class Nurse : Employee
     {
+        public int NumberOfPatients { get; set; }
 
+        public Nurse(string name, int eNumber, int numberOfPatients) : base(name, eNumber, 50000)
+        {
+            NumberOfPatients = numberOfPatients;
+        }
+
+        public override void PrintEmployees()
+        {
+            base.PrintEmployees();
+            Console.WriteLine("Number of Patients: " + NumberOfPatients);
+        }
+
+        public override void PayEmployee()
+        {
+            base.PayEmployee();
+            Console.WriteLine("Salary: " + base.Salary);
+        }
+
+        public override void DrawBlood(Patient currentPatient)
+        {
+            currentPatient.BloodLevel -= 2;
+            Console.WriteLine("The Nurse has drawn blood from Fragile Bob");
+            Console.WriteLine($"The patient's Blood Level is at { currentPatient.BloodLevel}");
+            Console.ReadLine();
+        }
+
+        public override void CarePatient(Patient currentPatient)
+        {
+            currentPatient.HealthLevel += 1;
+            Console.WriteLine("The Nurse has cared for Fragile Bob");
+            Console.WriteLine($"The patient's Health Level is at { currentPatient.HealthLevel}");
+            Console.ReadLine();
+
+        }
     }
-}
--- UCHHospital/Patient.cs
+++ UCHHospital/Patient.cs
@@ -6,5 +6,16 @@ namespace UCHHospital
 {
     class Patient
     {
+        public string PatientName { get; set; }
+        public int HealthLevel { get; set; }
+        public int BloodLevel { get; set; }
+
+        public Patient(string patientName)
+        {
+            PatientName = patientName;
+            HealthLevel = 10;
+            BloodLevel = 20;
+        }
+
     }
 }
